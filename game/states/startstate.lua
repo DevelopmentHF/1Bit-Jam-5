599b2e36c28fb4468fe7caef7d92b66b027aa8ef
Henry Fielding <hfielding@student.unimelb.edu.au>
2025-02-03 21:12:09 +1100

    Add highlighted and animated text capability

--- game/states/startstate.lua
+++ game/states/startstate.lua
@@ -1,11 +1,18 @@
 require("states.levelstate")
 require("states.state")
+require("ui.animatedtext")
 
 StartState = Class('StartState', State)
 
 -- TODO: Make this a singleton
+function StartState:initialize()
+	--self.title = HighlightedText:new(love.graphics.getWidth()/2 , love.graphics.getHeight()/2, "bit", 20)
+	
+	self.title = AnimatedText:new(love.graphics.getWidth()/2 , love.graphics.getHeight()/2, "1-bit Jam #5", BigFont, 10, 1)
+end
+
 function StartState:enter()
-    -- Initialize any data for the start state
+	
 end
 
 function StartState:update(dt)
@@ -13,19 +20,22 @@ function StartState:update(dt)
     if love.keyboard.isDown("space") then
         stateManager:switch(LevelState:new(1))
     end
+
+	self.title:update(dt)
 end
 
 function StartState:draw()
 	love.graphics.push()
     love.graphics.scale(ScalingFactor, ScalingFactor)
 
+	love.graphics.setColor(0, 0, 0)
     love.graphics.draw(Bg)
-
 
 	love.graphics.setColor(0, 0, 1, 1)
 	love.graphics.pop()
+	love.graphics.setColor(1, 1, 1, 1)
 
-    love.graphics.printf("1-BitJam", BigFont, 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
+	self.title:draw()
     love.graphics.printf("Press [Space] to Start", Font, 0, love.graphics.getHeight() - (love.graphics.getHeight() / 4), love.graphics.getWidth(), "center")
 
 end
